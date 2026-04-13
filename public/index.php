30822a7af108f16677e77fdcd63e30ec1eec6f96
Onimisi Adelza Adeolu <onimisiadeolu@gmail.com>
2026-04-13 15:57:23 +0100

    m

--- public/index.php
+++ public/index.php
@@ -13,6 +13,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
