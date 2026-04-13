6780d8fc3df3865320b9fb5fd5b17a649db88042
Onimisi Adelza Adeolu <onimisiadeolu@gmail.com>
2026-04-13 09:49:21 +0100

    added cors headers to index

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'allow_origin'      => '*',
+    'allow_credentials' => true,
+    'allow_methods'     => 'GET, POST, PUT, DELETE, OPTIONS',
+    'allow_headers'     => 'Content-Type, Authorization, X-Requested-With',
+    'exposed_headers'   => '',
+    'max_age'           => 1800,
+];
