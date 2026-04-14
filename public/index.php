897b9ebcd90cfbf60660b2c726608a3d8b5ad73b
Onimisi Adelza Adeolu <onimisiadeolu@gmail.com>
2026-04-15 00:33:37 +0100

    d

--- public/index.php
+++ public/index.php
@@ -11,8 +11,8 @@
 
 // CORS Headers
 // CORS FIX 
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, ngrok-skip-browser-warning');
 
 // IMPORTANT: force preflight stop BEFORE framework loads
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
