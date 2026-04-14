c04260dd8dea04481daf93b029534bb5ff61e413
Onimisi Adelza Adeolu <onimisiadeolu@gmail.com>
2026-04-14 11:16:44 +0100

    t

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
 // CORS Headers
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, ngrok-skip-browser-warning');
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
